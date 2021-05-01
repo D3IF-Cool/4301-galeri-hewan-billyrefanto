2024c16b3e3c521572850899273914df7c4c3534
billyrefanto <mochbillyrefanto@gmail.com>
2021-05-01 16:48:38 +0700

    3.5.Menampilkan gambar dari internet

--- app/src/main/java/org/d3if4041/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if4041/galerihewan/model/Hewan.kt
@@ -1,9 +1,7 @@
 package org.d3if4041.galerihewan.model
 
-import org.d3if4041.galerihewan.R
-
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int = R.drawable.angsa
+    val imageResId: String
 )
