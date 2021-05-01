0166689a9fb10379a46e3317e46119d1ae7d9459
billyrefanto <mochbillyrefanto@gmail.com>
2021-05-01 15:41:12 +0700

    3.2.Membuat view model untuk fitur main

--- app/src/main/java/org/d3if4041/galerihewan/ui/main/MainAdapter.kt
+++ app/src/main/java/org/d3if4041/galerihewan/ui/main/MainAdapter.kt
@@ -6,30 +6,37 @@ import androidx.recyclerview.widget.RecyclerView
 import org.d3if4041.galerihewan.databinding.ListItemBinding
 import org.d3if4041.galerihewan.model.Hewan
 
-class MainAdapter(private val data: List<Hewan>)
-    : RecyclerView.Adapter<MainAdapter.ViewHolder>()    {
+class MainAdapter: RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
-    class ViewHolder(private val binding: ListItemBinding):
-        RecyclerView.ViewHolder(binding.root){
-        fun bind(hewan: Hewan) = with(binding){
-            namaTextView.text = hewan.nama
+    private val data = mutableListOf<Hewan>()
+
+    fun updateData(newData: List<Hewan>){
+        data.clear()
+        data.addAll(newData)
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(private val binding: ListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(hewan: Hewan) = with(binding) {
+            namatextView.text = hewan.nama
             latinTextView.text = hewan.namaLatin
             imageView.setImageResource(hewan.imageResId)
-
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ListItemBinding.inflate(inflater,parent,false)
+        val binding = ListItemBinding.inflate(inflater, parent, false)
         return ViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(data[position])
     }
 
     override fun getItemCount(): Int {
         return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
     }
 }
